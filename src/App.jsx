70a57b13f460fbe343bbf53fa05df513ecc49cd0
1srah <s6603052413141@email.kmutnb.ac.th>
2025-03-07 23:02:33 +0700

    About page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { LoadingScreen } from "./components/LoadingScreen";
 import "./index.css";
 import { Navbar } from "./components/Navbar";
 import { MobileMenu } from "./components/MobileMenu";
+import { Home } from "./components/sections/Home";
+import { About } from "./components/sections/About";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -18,6 +20,8 @@ function App() {
       >
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+        <Home/>
+        <About/>
       </div>
     </>
   );
